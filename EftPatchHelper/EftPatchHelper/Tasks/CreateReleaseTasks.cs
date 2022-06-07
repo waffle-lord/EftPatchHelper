6f9ecf3e10a7a8e2cc9b28b46eebdfa8ace2e2e7
waffle.lord <waffle.lord@hotmail.com>
2022-06-07 19:50:08 -0400

    more things

---
+++ EftPatchHelper/EftPatchHelper/Tasks/CreateReleaseTasks.cs
@@ -0,0 +1,103 @@
+﻿using EftPatchHelper.Interfaces;
+using EftPatchHelper.Model;
+using Gitea.Api;
+using Gitea.Model;
+using Gitea.Client;
+using Spectre.Console;
+using EftPatchHelper.Extensions;
+
+namespace EftPatchHelper.Tasks
+{
+    public class CreateReleaseTasks : IReleaseCreator
+    {
+        private Settings _settings;
+        private Options _options;
+
+        public CreateReleaseTasks(Settings settings, Options options)
+        {
+            _settings = settings;
+            _options = options;
+        }
+
+        private bool UploadAsset(FileInfo file, Release release, RepositoryApi repo)
+        {
+            return AnsiConsole.Status().Start("Uploading Asset", (StatusContext context) =>
+            {
+                AnsiConsole.MarkupLine($"[blue]Adding release asset: {file.Name.EscapeMarkup()}[/]");
+
+                file.Refresh();
+
+                if (!file.Exists)
+                {
+                    AnsiConsole.MarkupLine($"[red]File does not exist: {file.FullName}[/]");
+                }
+
+                using var fileStream = file.OpenRead();
+
+                try
+                {
+                    var attachment = repo.RepoCreateReleaseAttachment(_settings.GiteaReleaseRepoOwner, _settings.GiteaReleaseRepoName, release.Id, fileStream, file.Name);
+
+                    AnsiConsole.MarkupLine("[green]Upload Complete[/]");
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    AnsiConsole.MarkupLine("[red]Failed to upload asset[/]");
+
+                    AnsiConsole.WriteException(ex);
+
+                    return false;
+                }
+            });
+        }
+
+        private Release? MakeRelease(RepositoryApi repo)
+        {
+            AnsiConsole.Write("Adding release to gitea ... ");
+
+            string sourceTail = _options.SourceClient.Version.Split('.').Last();
+
+            string targetTail = _options.TargetClient.Version.Split('.').Last();
+
+            string releaseName = $"{sourceTail} to {targetTail}";
+
+            try
+            {
+                var release = repo.RepoCreateRelease(_settings.GiteaReleaseRepoOwner, _settings.GiteaReleaseRepoName, new CreateReleaseOption(null, false, releaseName, false, sourceTail, null)); 
+
+                AnsiConsole.MarkupLine($"[green]Release added: {release.Name.EscapeMarkup()}[/]");
+
+                return release;
+            }
+            catch(Exception ex)
+            {
+                AnsiConsole.MarkupLine($"[red]Failed to create release[/]");
+
+                AnsiConsole.WriteException(ex);
+
+                return null;
+            }
+        }
+
+        public void Run()
+        {
+            AnsiConsole.WriteLine();
+
+            if (!_options.CreateRelease) return;
+
+            Configuration.Default.BasePath = _settings.GiteaApiBasePath;
+
+            Configuration.Default.AddApiKey("token", _settings.GiteaApiKey);
+
+            var repo = new RepositoryApi(Configuration.Default);
+
+            var release = MakeRelease(repo).ValidateOrExit<Release>();
+
+            var fileInfo = new FileInfo(_options.OutputPatchPath + ".zip");
+
+            UploadAsset(fileInfo, release, repo);
+        }
+    }
+}
---
+++ EftPatchHelper/EftPatchHelper/Tasks/PatchGenTasks.cs
@@ -0,0 +1,92 @@
+﻿using EftPatchHelper.Extensions;
+using EftPatchHelper.Interfaces;
+using EftPatchHelper.Model;
+using Spectre.Console;
+using System.Diagnostics;
+
+namespace EftPatchHelper.Tasks
+{
+    internal class PatchGenTasks : IPatchGenTasks
+    {
+        private Settings _settings;
+        private Options _options;
+
+        public PatchGenTasks(Settings settings, Options options)
+        {
+            _settings = settings;
+            _options = options;
+        }
+
+        private bool RunPatchGenerator()
+        {
+            if (!File.Exists(_settings.PatcherEXEPath))
+            {
+                AnsiConsole.WriteLine("Could not find patch generator exe");
+                return false;
+            }
+
+            string patcherOutputName = $"Patcher_{_options.SourceClient.Version}_to_{_options.TargetClient.Version}";
+
+            var patcherPath = new FileInfo(_settings.PatcherEXEPath)?.Directory?.FullName;
+
+            if(patcherPath == null)
+            {
+                AnsiConsole.WriteLine("Could not find patch generator folder");
+                return false;
+            }
+
+            _options.OutputPatchPath = Path.Join(patcherPath, patcherOutputName);
+
+            return AnsiConsole.Status().Start("Staring Patch Generator ...", (StatusContext context) =>
+            {
+                var genProc = Process.Start(new ProcessStartInfo()
+                {
+                    FileName = _settings.PatcherEXEPath,
+                    WorkingDirectory = new FileInfo(_settings.PatcherEXEPath).Directory?.FullName ?? Directory.GetCurrentDirectory(),
+                    ArgumentList =
+                    {
+                        $"OutputFolderName::{patcherOutputName}",
+                        $"SourceFolderPath::{_options.SourceClient.PrepPath}",
+                        $"TargetFolderPath::{_options.TargetClient.PrepPath}",
+                        $"AutoZip::{_settings.AutoZip}",
+                        $"AutoClose::{_settings.AutoClose}"
+                    }
+                });
+
+
+                context.Status = "Generating patches ...";
+
+                genProc?.WaitForExit();
+
+                switch ((PatcherExitCode)genProc.ExitCode)
+                {
+                    case PatcherExitCode.ProgramClosed:
+                        {
+                            AnsiConsole.MarkupLine("[red]Program was closed[/]");
+                            return false;
+                        }
+                    case PatcherExitCode.Success:
+                        {
+                            AnsiConsole.MarkupLine("[green]Patches Generated[/]");
+                            return true;
+                        }
+                    case PatcherExitCode.MissingDir:
+                        {
+                            AnsiConsole.MarkupLine("[red]Missing Dir[/]");
+                            return false;
+                        }
+                    default:
+                        {
+                            AnsiConsole.MarkupLine("[red]Something went wrong[/]");
+                            return false;
+                        }
+                }
+            });
+        }
+
+        public void Run()
+        {
+            RunPatchGenerator().ValidateOrExit();
+        }
+    }
+}
